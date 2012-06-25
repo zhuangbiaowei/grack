4472e59685414be4751c88a0545771f84baea5bd
Saito <saitowu@gmail.com>
2012-06-25 18:24:31 +0800

    refactor instance method to new

--- lib/grack/bundle.rb
+++ lib/grack/bundle.rb
@@ -7,15 +7,15 @@ module Grack
     extend self
 
     @@__instance__ = nil
+    @@__config__ = YAML.load_file("config/grack.yml")
 
-    def instance
+    def new
       return @@__instance__ if @@__instance__
       @@__instance__ = Rack::Builder.new do
         use Grack::Auth do |username, password|
           false
         end
 
-        @@__config__ ||= YAML.load_file("config/grack.yml")
         run Grack::Server.new(@@__config__)
       end
     end
