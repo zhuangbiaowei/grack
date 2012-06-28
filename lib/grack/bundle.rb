dd990d231ac9189046b71a65d874638e55320e27
Saito <saitowu@gmail.com>
2012-06-28 14:40:39 +0800

    refactor bundle code, much cleaner for read and easy to use

--- lib/grack/bundle.rb
+++ lib/grack/bundle.rb
@@ -6,18 +6,15 @@ module Grack
   module Bundle
     extend self
 
-    @@__instance__ = nil
-    @@__config__ = YAML.load_file("config/grack.yml")
-
-    def new
-      return @@__instance__ if @@__instance__
-      @@__instance__ = Rack::Builder.new do
+    def new(config)
+      Rack::Builder.new do
         use Grack::Auth do |username, password|
           false
         end
 
-        run Grack::Server.new(@@__config__)
+        run Grack::Server.new(config)
       end
     end
+
   end
 end
